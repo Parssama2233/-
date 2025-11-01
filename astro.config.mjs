a0c46b2b1b5ba5d546bfb24c0188af7e35762a7d
Parssama2233 <hello.Pars@outlook.com>
2025-11-01 12:30:48 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,7 +28,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
-	site: "https://windholm.dpdns.org",
+	site: "https://windholm.pages.dev",
 
 	base: "/",
 	trailingSlash: "always",
